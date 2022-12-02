4d13ac875c205c83101d55f75d344c7fc52f9500
Chris Gabler <christopher.gabler@allaboutapps.at>
2022-12-02 14:11:12 +0100

    fix open app store, other improvements

--- README.md
+++ README.md
@@ -1,39 +1,15 @@
-<!-- 
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
--->
-
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+AllAboutForcedUpdate
 
 ## Features
 
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+Check if the current version of the app is outdated and needs an update.
+Provide an url to a .json file with the following contents:
 
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
-
-```dart
-const like = 'sample';
+```json
+{
+  "minVersionCodeAndroid": 0,
+  "minVersionCodeIos": 0,
+  "iosAppStoreId": "123456789",
+  "androidPackageName": "at.allaboutapps.app"
+}
 ```
-
-## Additional information
-
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
---
+++ lib/all_about_forced_update.dart
@@ -0,0 +1,52 @@
+library all_about_forced_update;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:open_store/open_store.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class AllAboutForcedUpdate {
+  AllAboutForcedUpdate._();
+
+  static final AllAboutForcedUpdate _instance = AllAboutForcedUpdate._();
+
+  static AllAboutForcedUpdate get instance => _instance;
+
+  String? _iosAppStoreId;
+  String? _androidPackageName;
+
+  Future<bool> forcedUpdateNeeded(String updateConfigUrl) async {
+    try {
+      int? localVersion = int.tryParse((await PackageInfo.fromPlatform()).buildNumber);
+
+      Uri url = Uri.parse(updateConfigUrl);
+      var response = await http.get(url).timeout(const Duration(seconds: 2));
+      var json = jsonDecode(response.body);
+
+      int? onlineVersion = Platform.isAndroid ? json['minVersionCodeAndroid'] : json['minVersionCodeIos'];
+      _iosAppStoreId = json['iosAppStoreId'];
+      _androidPackageName = json['androidPackageName'];
+      if (localVersion != null && onlineVersion != null && _iosAppStoreId != null && _androidPackageName != null) {
+        if (localVersion < onlineVersion) {
+          return true;
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Download of .json file or parsing failed: $e');
+      }
+    }
+    return false;
+  }
+
+  Future<void> openAppStore() async {
+    if (_iosAppStoreId == null || _androidPackageName == null) return;
+    await OpenStore.instance.open(
+      appStoreId: _iosAppStoreId,
+      androidAppBundleId: _androidPackageName,
+    );
+  }
+}
